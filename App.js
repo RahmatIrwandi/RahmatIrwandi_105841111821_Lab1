c80f8f32c1ace1683f29c0ceff3d0f0fa3e05c61
RahmatIrwandi <105841111821@student.unismuh.ac.id.com>
2024-06-25 01:47:01 +0800

    text input

--- App.js
+++ App.js
@@ -1,82 +1,39 @@
 
-import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+import { Text, View, Image, TextInput } from 'react-native';
 import React from 'react';
-import ButtonComponent from './component/button/button';
 
 const App = () => {
+  const Inputan = ({ nama, color }) => {
+  return (
+   <TextInput
+    placeholder= {`masukkan ${nama}`}
+    style= {{
+      borderWidth: 2,
+      borderColor: 'black',
+      borderRadius: 15,
+      width: 340,
+      height: 60,
+      marginVertical: 15,
+      padding: 15,
+      backgroundColor: 'white',
+      color: color,
+     }}
+    />
+  );
+};
+
   return (
     <View style= {{
       flex: 1,
-      backgroundColor: '#000',
-    }}>
-      <Image
-        source={require('./assets/WandStore.png')}
-        style={{
-         width: '120%',
-         height: '100%',
-         position: 'absolute',
-         top: 25,
-         opacity: 0.5,
-         zIndex: -1,
-        }}
-        resizeMode='cover'
-      />
-      <ScrollView contentContainerStyle={{
-        flexGrow: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: 20,
-      }}>
-        <Image
-          source={require('./assets/WandStore.png')}
-          style={{
-            width: 240,
-            height: 100,
-            marginTop: 5,
-            opacity: 1,
-            zIndex: 1,
-          }}
-          resizeMode= 'cover'
-        />
-        <Text style={{
-          fontSize: 30,
-          fontFamily: 'sans-serif',
-          fontWeight: 'bold',
-          color: 'white',
-          position: 'absolute',
-          top: 0,
-          marginTop: 150,
-        }}>Welcome to WandStore</Text>
-
-        <Text style={{
-          color: 'white',
-          fontSize: 20,
-          textAlign: 'center',
-          marginTop: 10,
-        }}>WandStore adalah toko fashion kekininian</Text>
-
-        <Text style={{
-          color: 'white',
-          fontSize: 18,
-          textAlign: 'center',
-          marginTop: 10,
-        }}>pilih fashion sesuai kebutuhan kamu </Text>
-        <View style={{
-          flexDirection: 'row',
-          justifyContent: 'center',
-          alignItems: 'center',
-          marginTop: 20,
-          marginBottom: 50,
-        }}>
-          
-          <ButtonComponent title= "Sign In" color= "green" />
-          <ButtonComponent title= "Sign Up" color= "grey" />
-         </View>
-        </ScrollView>
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}>    
+        <Inputan nama= "Email" color = "black" />
+        <Inputan nama= "Username" color = "black" />
+        <Inputan nama= "Password" color = "black" />
       </View>
     );
-  }
- 
+  };
 
 export default App;
 
